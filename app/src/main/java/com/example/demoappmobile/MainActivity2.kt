abf2269929b029869e98dbd5b5c83060daea48d0
Elmosius Suli <elmosius18@gmail.com>
2025-09-25 20:25:23 +0700

    feat: create exercise 2

---
+++ app/src/main/java/com/example/demoappmobile/MainActivity2.kt
@@ -0,0 +1,74 @@
+package com.example.demoappmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.demoappmobile.ui.theme.DemoAppMobileTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DemoAppMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Exercise2(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+// Exercise 2
+@Composable
+fun Exercise2(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.test),
+            contentDescription = null,
+            modifier = modifier
+                .width(250.dp)
+                .height(250.dp)
+        )
+        Text(
+            text = "All tasks completed",
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    Exercise2()
+}
