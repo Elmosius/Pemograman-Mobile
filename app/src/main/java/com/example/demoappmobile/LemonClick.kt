8ccd8da4afdc08763afc4b83580a615837498e47
Elmosius Suli <elmosius18@gmail.com>
2025-10-02 16:18:31 +0700

    feat: exercise 02

--- app/src/main/java/com/example/demoappmobile/LemonClick.kt
+++ app/src/main/java/com/example/demoappmobile/LemonClick.kt
@@ -1,17 +1,16 @@
 package com.example.demoappmobile
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -20,16 +19,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.demoappmobile.entity.Student
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.demoappmobile.ui.theme.DemoAppMobileTheme
 
-class Topic3 : ComponentActivity() {
+class LemonClick : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -41,20 +46,13 @@ class Topic3 : ComponentActivity() {
                         TopBar()
                     }
                 ) { innerPadding ->
-                    AppBarDemo(modifier = Modifier.padding(innerPadding))
-//                    ButtonExample(modifier = Modifier.padding(innerPadding))
+                    AppContent(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-private fun showStudentInfo(context: Context) {
-    val student = Student("0001", "John", "Doe")
-    Toast.makeText(
-        context, student.showInfo(), Toast.LENGTH_SHORT
-    ).show()
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,18 +60,19 @@ private fun TopBar() {
     CenterAlignedTopAppBar(
         title = {
             Text(
-                text = stringResource(R.string.app_name)
+                text = stringResource(R.string.app_name_lemon_click),
+                fontWeight = FontWeight.Bold,
             )
         },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.White,
+            containerColor = Color(0xfff6eb74),
         )
     )
 }
 
 @Composable
-private fun AppBarDemo(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
+private fun AppContent(modifier: Modifier = Modifier) {
+    var step by remember { mutableIntStateOf(1) }
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -81,44 +80,58 @@ private fun AppBarDemo(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { showStudentInfo(context) },
+            onClick = {
+                step = step % 4 + 1
+            },
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xff5dbb6d)
-            )
+                containerColor = Color(0xffffd7e1),
+            ),
+            shape = RoundedCornerShape(40.dp),
         ) {
-            Text(text = "Show Student Info")
+            ShowImage(step)
         }
+        ShowText(step)
     }
 }
 
 @Composable
-private fun ButtonExample(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                Color(0xff4f84f9)
-            ),
-    ) {
-        Column(
-            modifier = Modifier.align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(
-                onClick = {
-                    showStudentInfo(context)
-                }
-            ) {
-                Text(text = "Hello")
-            }
-        }
+private fun ShowImage(step: Int) {
+    var image = R.drawable.lemon_tree
+    when (step) {
+        1 -> image = R.drawable.lemon_tree
+        2 -> image = R.drawable.lemon_squeeze
+        3 -> image = R.drawable.lemon_drink
+        4 -> image = R.drawable.lemon_restart
     }
+
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = "Lemon step $step",
+        modifier = Modifier
+            .width(150.dp)
+            .padding(20.dp)
+    )
+}
+
+@Composable
+fun ShowText(step: Int) {
+    var text = stringResource(R.string.step1)
+    when (step) {
+        1 -> text = stringResource(R.string.step1)
+        2 -> text = stringResource(R.string.step2)
+        3 -> text = stringResource(R.string.step3)
+        4 -> text = stringResource(R.string.step4)
+    }
+
+    Text(
+        text = text,
+        modifier = Modifier.padding(20.dp),
+        fontSize = 15.sp
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview3() {
-    ButtonExample()
+fun GreetingPreview2() {
+    AppContent()
 }
