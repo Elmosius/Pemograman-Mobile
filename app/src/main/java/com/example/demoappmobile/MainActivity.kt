c5a9f3d2bdf0620aa17abc45409c8bbae96462cc
Elmosius Suli <elmosius18@gmail.com>
2025-11-10 13:13:19 +0700

    feat: intial project

--- app/src/main/java/com/example/demoappmobile/MainActivity.kt
+++ app/src/main/java/com/example/demoappmobile/MainActivity.kt
@@ -6,13 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.demoappmobile.components.pages.DetailPage
-import com.example.demoappmobile.components.pages.MainPage
 import com.example.demoappmobile.ui.theme.DemoAppMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,37 +22,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
-    val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = "home"
-    ) {
-        composable("home") {
-            MainPage(
-                onMovieClick = { movieId ->
-                    navController.navigate("detail/$movieId")
-                }
-            )
-        }
-
-        composable(
-            route = "detail/{movieId}",
-            arguments = listOf(
-                navArgument("movieId") {
-                    type = NavType.IntType
-                }
-            )
-        ) { backStackEntry ->
-            val movieId = backStackEntry.arguments?.getInt("movieId") ?: 0
-            DetailPage(
-                movieId = movieId,
-                onBackClick = {
-                    navController.popBackStack()
-                }
-            )
-        }
-    }
 }
 
 
--- app/src/main/java/com/example/demoappmobile/models/Movie.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demoappmobile.models
-
-data class Movie(
-    var id: Int,
-    var judul: String,
-    var deskripsi: String,
-    var poster: String
-)
